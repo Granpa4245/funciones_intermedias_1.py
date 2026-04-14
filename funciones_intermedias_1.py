04bfd8440753f871466233b1480a94a55d0a5ce8
FacundoVazquez <zamoranofacu4245@gmail.com>
2026-04-14 19:32:26 -0300

    Update funciones_intermedias_1.py

--- funciones_intermedias_1.py
+++ funciones_intermedias_1.py
@@ -17,11 +17,23 @@ coordenadas[0]["latitud"] = 9.9355431
 
 #2
 
+cantantes2 = [
+    {"nombre": "Ricky Martin", "pais": "Puerto Rico"},
+    {"nombre": "Chayanne", "pais": "Puerto Rico"},
+    {"nombre": "José José", "pais": "México"},
+    {"nombre": "Juan Luis Guerra", "pais": "República Dominicana"}
+]
+
+
 def iterarDiccionario(lista):
-    for diccionario in lista:
-        for llave, valor in diccionario.items():
-            print(f"{llave} - {valor}", end=", ")
-        print()  
+    for i in lista:
+        pares = []
+        for llave, valor in i.items():
+            pares.append(f"{llave} - {valor}")
+
+        print(", ".join(pares))
+
+iterarDiccionario(cantantes2)
 
 #3
 
